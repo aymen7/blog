5bb6aed5f047b7eaf7b0c1de44f3c8be72f468b5
aymen7 <aymenokay@gmail.com>
2017-10-11 18:18:02 +0100

    orgonise the file structure
    create a directory and put in it all the files related to the index page

---
+++ php/index-main-content-files/main-column.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 11/10/2017
+ * Time: 17:21
+ */
+?>
+<div id="main-column" class="col-lg-8">
+    <?php require("post_script.php");
+    foreach ($result as $row)
+    {
+        echo '<div class="post">
+                <img src="'.$row['img_path'].'" alt="post image">
+                <div class="title_body-wrapper">
+                <h2 class="post-title">'.$row['title'].'</h2>
+                <p class="post-body-teaser">'.$row['preview'].'</p>
+                </div><!-- title_body-wrapper -->';
+        echo '<div class="date_tag-wrapper">
+                <div class="date-div">
+                    <span><i>on </i></span><h3>'.$row['date'].'</h3>
+                </div>
+                <div class="tag-div">
+                    <span><i>in </i></span><h3><a href="#">'.$row['tag'].'</a></h3>
+                </div>
+            </div><!-- date_tag-wrapper -->';
+
+        echo '<div class="author-div">
+                <span><i>by </i></span><h3><a href="#">'.$row['author'].'</a></h3>
+            </div>';
+        echo '<a href="#" class="btn btn-default">Continue Reading</a>';
+        echo ' </div><!-- post----->';
+
+    }
+    ?>
+</div><!-- end of main column-->
--- php/main-content.php
+++ php/main-content.php
@@ -1,34 +1,12 @@
 <section class="container">
-    <div id="main-column" class="col-lg-8">
-        <?php require ("post_script.php");
-        foreach ($result as $row)
-        {
-            echo '<div class="post">
-                <img src="'.$row['img_path'].'" alt="post image">
-                <div class="title_body-wrapper">
-                <h2 class="post-title">'.$row['title'].'</h2>
-                <p class="post-body-teaser">'.$row['preview'].'</p>
-                </div><!-- title_body-wrapper -->';
-            echo '<div class="date_tag-wrapper">
-                <div class="date-div">
-                    <span><i>on </i></span><h3>'.$row['date'].'</h3>
-                </div>
-                <div class="tag-div">
-                    <span><i>in </i></span><h3><a href="#">'.$row['tag'].'</a></h3>
-                </div>
-            </div><!-- date_tag-wrapper -->';
 
-            echo '<div class="author-div">
-                <span><i>by </i></span><h3><a href="#">'.$row['author'].'</a></h3>
-            </div>';
-            echo '<a href="#" class="btn btn-default">Continue Reading</a>';
-            echo ' </div><!-- post----->';
-            
-        }
+    <?php
+        //include main column
+        require 'index-main-content-files/main-column.php';
         //include pagination
-        require 'pagination.php';
+        require 'index-main-content-files/pagination.php';
         //include aside
-        require 'aside.php';
+        require 'index-main-content-files/aside.php';
 
 
         ?>
